549eabfc899485ad0bba963467d8c61b13e36dd6
FishyFoshy <nickwar2004@gmail.com>
2025-01-31 17:32:18 -0400

    gfds

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ def get_students():
     return jsonify(data)    # return student data in response
 
 @app.route('/students/<id>')
-def get_students():
-    return jsonify(data)# return student data in response   
+def get_student(id):
+  for student in data: 
+    if student['id'] == id: # filter out the students without the specified id
+      return jsonify(student)
 
 app.run(host='0.0.0.0', port=8080, debug=True)
