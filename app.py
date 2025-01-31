b7d7a01dffb13aa2c43953dc0686d3cd538b1cab
FishyFoshy <nickwar2004@gmail.com>
2025-01-31 17:50:21 -0400

    exercise 1

--- app.py
+++ app.py
@@ -30,5 +30,41 @@ def get_student(id):
   for student in data: 
     if student['id'] == id: # filter out the students without the specified id
       return jsonify(student)
+    
+@app.route('/stats')
+def get_stats():
+  chick = 0
+  fish = 0
+  veg = 0
+  compSciM = 0
+  compSciS = 0
+  InfoM = 0
+  InfoS = 0
+  for student in data:
+    if student['pref'] == 'Chicken':
+      chick += 1
+    if student['pref'] == 'Fish':
+      fish += 1
+    if student['pref'] == 'Vegetable':
+      veg += 1
+    if student['programme'] == 'Computer Science (Major)':
+      compSciM += 1
+    if student['programme'] == 'Computer Science (Special)':
+      compSciS += 1
+    if student['programme'] == 'Information Technology (Major)':
+      InfoM += 1
+    if student['programme'] == 'Information Technology (Special)':
+      InfoS += 1
+  
+  stat = {
+        "Chicken":chick,
+        "Computer Science (Major)":compSciM,
+        "Computer Science (Special)":compSciS,
+        "Fish":fish,
+        "Information Technology (Major)":InfoM,
+        "Information Technology (Special)":InfoS,
+        "Vegetable":veg
+  }
+  return stat
 
 app.run(host='0.0.0.0', port=8080, debug=True)
